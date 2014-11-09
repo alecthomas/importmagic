48f1ad516844c54a6e0cd2b412d7297d2bf324da
Georg Brandl <georg@python.org>
2014-11-09 11:10:33 +0100

    Fix visit_Raise on Python 3 and add a test.
    Also raise test coverage of symbols_test a bit.

--- importmagic/symbols.py
+++ importmagic/symbols.py
@@ -180,12 +180,18 @@ class UnknownSymbolVisitor(ast.NodeVisitor):
             self.generic_visit(node)
 
     def visit_Raise(self, node):
-        with self._scope.start_reference():
-            self.visit(node.type)
-        with self._scope.start_reference():
-            self.visit(node.inst)
-        with self._scope.start_reference():
-            self.visit(node.tback)
+        if hasattr(node, 'type'):  # Python 2: raise A[, B[, C]]
+            with self._scope.start_reference():
+                self.visit(node.type)
+            with self._scope.start_reference():
+                self.visit(node.inst)
+            with self._scope.start_reference():
+                self.visit(node.tback)
+        else:                      # Python 3: raise A[ from B]
+            with self._scope.start_reference():
+                self.visit(node.exc)
+            with self._scope.start_reference():
+                self.visit(node.cause)
 
     def visit_TryExcept(self, node):
         for sub in node.body:
