6952ce726f44bda5d00ae0e75ca4767c6a631730
Elric Milon <whirm@gmx.com>
2015-06-11 10:46:36 +0200

    Test for max columns configuration.

--- importmagic/importer_test.py
+++ importmagic/importer_test.py
@@ -237,7 +237,7 @@ def test_from_import_as(index):
 
 
 def test_importer_wrapping_escaped(index):
-    Imports.set_style(multiline='backslash')
+    Imports.set_style(multiline='backslash', max_columns=80)
     src = dedent('''
         from injector import Binder, Injector, InstanceProvider, Key, MappingKey, Module, Scope, ScopeDecorator, SequenceKey, inject, provides, singleton
         from waffle import stuff
@@ -258,7 +258,7 @@ def test_importer_wrapping_escaped(index):
     assert expected_src == new_src
 
 def test_importer_wrapping_escaped_longer(index):
-    Imports.set_style(multiline='backslash')
+    Imports.set_style(multiline='backslash', max_columns=80)
     src = dedent('''
         from injector import Binder, Injector, InstanceProvider, Key, MappingKey, Module, Scope, ScopeDecorator, SequenceKey, inject, provides, singleton, more, things, imported, foo, bar, baz, cux, lorem, ipsum
         from waffle import stuff
@@ -280,7 +280,7 @@ def test_importer_wrapping_escaped_longer(index):
     assert expected_src == new_src
 
 def test_importer_wrapping_parentheses(index):
-    Imports.set_style(multiline='parentheses')
+    Imports.set_style(multiline='parentheses', max_columns=80)
     src = dedent('''
         from injector import Binder, Injector, InstanceProvider, Key, MappingKey, Module, Scope, ScopeDecorator, SequenceKey, inject, provides, singleton
         from waffle import stuff
@@ -302,7 +302,7 @@ def test_importer_wrapping_parentheses(index):
 
 
 def test_importer_wrapping_parentheses_longer(index):
-    Imports.set_style(multiline='parentheses')
+    Imports.set_style(multiline='parentheses', max_columns=80)
     src = dedent('''
         from injector import Binder, Injector, InstanceProvider, Key, MappingKey, Module, Scope, ScopeDecorator, SequenceKey, inject, provides, singleton, more, things, imported, foo, bar, baz, cux, lorem, ipsum
         from waffle import stuff
@@ -322,6 +322,30 @@ def test_importer_wrapping_parentheses_longer(index):
     scope = Scope.from_source(src)
     new_src = update_imports(src, index, *scope.find_unresolved_and_unreferenced_symbols()).strip()
     assert expected_src == new_src
+
+
+def test_importer_wrapping_colums(index):
+    Imports.set_style(multiline='parentheses', max_columns=120)
+    src = dedent('''
+        from injector import Binder, Injector, InstanceProvider, Key, MappingKey, Module, Scope, ScopeDecorator, SequenceKey, inject, provides, singleton, more, things, imported, foo, bar, baz, cux, lorem, ipsum
+        from waffle import stuff
+
+        Binder, Injector, InstanceProvider, Key, MappingKey, Module, Scope, ScopeDecorator, SequenceKey, inject, provides, singleton, more, things, imported, foo, bar, baz, cux, lorem, ipsum, stuff
+        ''').strip()
+    expected_src = dedent('''
+        from injector import (Binder, Injector, InstanceProvider, Key, MappingKey, Module, Scope, ScopeDecorator, SequenceKey,
+            bar, baz, cux, foo, imported, inject, ipsum, lorem, more, provides, singleton, things)
+        from waffle import stuff
+
+
+        Binder, Injector, InstanceProvider, Key, MappingKey, Module, Scope, ScopeDecorator, SequenceKey, inject, provides, singleton, more, things, imported, foo, bar, baz, cux, lorem, ipsum, stuff
+        ''').strip()
+
+    scope = Scope.from_source(src)
+    new_src = update_imports(src, index, *scope.find_unresolved_and_unreferenced_symbols()).strip()
+
+    assert expected_src == new_src
+
 
 def test_importer_directives(index):
     src = dedent('''
